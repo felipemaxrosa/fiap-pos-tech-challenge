40f2eddb77989542b62bd6ff0dc59fd95f68aee7
Renato Rodrigues <spamcares-github@yahoo.com>
2023-06-13 19:34:56 -0300

    Fechando versão estável

--- scripts/hooks/pre-commit.sh
+++ scripts/hooks/pre-commit.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 
-# Executa os testes unitarios e e2e
-NODE_ENV=local-mock-repository
+# Configura para usar o repositorio mockado
+NODE_ENV_CURRENT=${NODE_ENV}
+export NODE_ENV='local-mock-repository'
 
+# Executa os testes unitarios e e2e
 echo "Executando testes unitários.."
 npm run test
 
 echo "Executando testes de de integração (modo local moked).."
 npm run test:e2e
+
+# Restaura os valores originais
+export NODE_ENV=${NODE_ENV_CURRENT}
+unset NODE_ENV_CURRENT
 
 status=$?
 
