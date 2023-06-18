56268ef27d69904334331758255f605989ab7487
Felipe Maximiliano da Rosa <fmo19@bskyb.com>
2023-06-18 13:20:05 +0100

    chore: typeorm database config

--- src/infrastructure/database/typeorm-database.module.ts
+++ src/infrastructure/database/typeorm-database.module.ts
@@ -5,18 +5,26 @@ import { MysqlConfg } from './mysql/mysql.config';
 import { DatabaseConstants } from './mysql/mysql.constants';
 import { ClienteEntity } from './cliente/entity/cliente.entity';
 import { ClienteTypeormRepository } from './cliente/repository/cliente-typeorm.repository';
-
+import { PedidoEntity } from './cliente/entity/pedido.entity';
+import { PedidoTypeormRepository } from './cliente/repository/pedido-typeorm.repository';
+import { PedidoConstants } from 'src/shared/constants';
 @Module({
    imports: [
       DatabaseConstants,
-      TypeOrmModule.forFeature([ClienteEntity]),
+      TypeOrmModule.forFeature([ClienteEntity, PedidoEntity]),
       TypeOrmModule.forRootAsync({
          imports: [MysqlConfg],
          useFactory: async (config: TypeOrmModuleOptions) => config,
          inject: [DatabaseConstants.DATABASE_CONFIG_NAME],
       }),
    ],
-   providers: [{ provide: 'IRepository<Cliente>', useClass: ClienteTypeormRepository }],
-   exports: [{ provide: 'IRepository<Cliente>', useClass: ClienteTypeormRepository }],
+   providers: [
+      { provide: 'IRepository<Cliente>', useClass: ClienteTypeormRepository },
+      { provide: PedidoConstants.IREPOSITORY, useClass: PedidoTypeormRepository },
+   ],
+   exports: [
+      { provide: 'IRepository<Cliente>', useClass: ClienteTypeormRepository },
+      { provide: PedidoConstants.IREPOSITORY, useClass: PedidoTypeormRepository },
+   ],
 })
 export class TypeormDatabaseModule {}
