93455bbf17c9798ebcd9b473614c7aa4c04388f5
Felipe Maximiliano da Rosa <fmo19@bskyb.com>
2023-06-18 10:45:47 +0100

    chore: pedido controller

---
+++ src/application/web/pedido/controller/pedido.controller.ts
@@ -0,0 +1,33 @@
+import { Body, Controller, Inject, Logger, Post } from '@nestjs/common';
+import { ApiConsumes, ApiCreatedResponse, ApiProduces, ApiTags } from '@nestjs/swagger';
+
+import { Pedido } from 'src/domain/pedido/model/pedido.model';
+import { IService } from 'src/domain/service/service';
+import { CriarNovoPedidoRequest } from '../request/criar-novo-pedido.request';
+import { ESTADO_PEDIDO } from 'src/domain/pedido/enums/pedido';
+
+@Controller('v1/pedido')
+@ApiTags('Pedido')
+@ApiConsumes('application/json')
+@ApiProduces('application/json')
+export class PedidoController {
+   private logger: Logger = new Logger(PedidoController.name);
+
+   constructor(@Inject('IService<Pedido>') private service: IService<Pedido>) {}
+
+   @Post()
+   @ApiCreatedResponse({ description: 'Pedido gerado com sucesso' })
+   async salvar(@Body() request: CriarNovoPedidoRequest): Promise<Pedido> {
+      this.logger.debug(`Criando Novo Pedido Request: ${JSON.stringify(request)}`);
+      return await this.service
+         .save({
+            clienteId: request.clienteId,
+            dataInicio: request.dataInicio,
+            estadoPedido: ESTADO_PEDIDO.EM_PREPARO,
+         })
+         .then((pedido) => {
+            this.logger.log(`Pedido gerado com sucesso: ${pedido.id}}`);
+            return pedido;
+         });
+   }
+}
---
+++ src/application/web/pedido/request/criar-novo-pedido.request.ts
@@ -0,0 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsNumberString, IsString } from 'class-validator';
+
+export class CriarNovoPedidoRequest {
+   @ApiProperty({ required: true, nullable: false, description: 'ID do cliente' })
+   @IsNotEmpty({ message: 'ID do cliente não pode ser vazio' })
+   @IsNumberString({}, { message: 'ID do cliente deve ser válido' })
+   public clienteId: number;
+
+   @ApiProperty({ required: true, nullable: false, description: 'Data e Hora do Inicio do Pedido' })
+   @IsNotEmpty({ message: 'Data e Hora não pode ser vazio' })
+   @IsString({ message: 'Data e Hora deve ser válido' })
+   public dataInicio: string;
+
+   @ApiProperty({ required: true, nullable: false, description: 'Estado do pedido' })
+   @IsNotEmpty({ message: 'Estado do pedido não pode ser vazio' })
+   @IsString({ message: 'Estado do pedido deve ser válido' })
+   public estadoPedido: string;
+}
