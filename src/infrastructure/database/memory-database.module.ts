bc049730a3e90ac7d061dd06941d43fcc5f84886
Rodrigo <ottero@gmail.com>
2023-06-18 20:51:54 +0100

    Crud Produtos: WIP: início da criação da feature

--- src/infrastructure/database/memory-database.module.ts
+++ src/infrastructure/database/memory-database.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 
 import { ClienteMemoryRepository } from './cliente/repository/cliente-memory.repository';
+import { ProdutoMemoryRepository } from './produto/repository/produto-memory.repository';
 
 @Module({
-   providers: [{ provide: 'IRepository<Cliente>', useClass: ClienteMemoryRepository }],
-   exports: [{ provide: 'IRepository<Cliente>', useClass: ClienteMemoryRepository }],
+   providers: [
+      { provide: 'IRepository<Cliente>', useClass: ClienteMemoryRepository },
+      { provide: 'IRepository<Produto>', useClass: ProdutoMemoryRepository },
+   ],
+   exports: [
+      { provide: 'IRepository<Cliente>', useClass: ClienteMemoryRepository },
+      { provide: 'IRepository<Produto>', useClass: ProdutoMemoryRepository },
+   ],
 })
 export class MemoryDatabaseModule {}
--- src/infrastructure/database/typeorm-database.module.ts
+++ src/infrastructure/database/typeorm-database.module.ts
@@ -5,18 +5,26 @@ import { MysqlConfg } from './mysql/mysql.config';
 import { DatabaseConstants } from './mysql/mysql.constants';
 import { ClienteEntity } from './cliente/entity/cliente.entity';
 import { ClienteTypeormRepository } from './cliente/repository/cliente-typeorm.repository';
+import { ProdutoEntity } from './produto/entity/produto.entity';
+import { ProdutoTypeormRepository } from './produto/repository/produto-typeorm.repository';
 
 @Module({
    imports: [
       DatabaseConstants,
-      TypeOrmModule.forFeature([ClienteEntity]),
+      TypeOrmModule.forFeature([ClienteEntity, ProdutoEntity]),
       TypeOrmModule.forRootAsync({
          imports: [MysqlConfg],
          useFactory: async (config: TypeOrmModuleOptions) => config,
          inject: [DatabaseConstants.DATABASE_CONFIG_NAME],
       }),
    ],
-   providers: [{ provide: 'IRepository<Cliente>', useClass: ClienteTypeormRepository }],
-   exports: [{ provide: 'IRepository<Cliente>', useClass: ClienteTypeormRepository }],
+   providers: [
+      { provide: 'IRepository<Cliente>', useClass: ClienteTypeormRepository },
+      { provide: 'IRepository<Produto>', useClass: ProdutoTypeormRepository },
+   ],
+   exports: [
+      { provide: 'IRepository<Cliente>', useClass: ClienteTypeormRepository },
+      { provide: 'IRepository<Produto>', useClass: ProdutoTypeormRepository },
+   ],
 })
 export class TypeormDatabaseModule {}
