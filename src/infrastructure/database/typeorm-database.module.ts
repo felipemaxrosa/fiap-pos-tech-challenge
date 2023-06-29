835481a6abbb974e969fb862dccac3355bb04307
Felipe Maximiliano da Rosa <fmo19@bskyb.com>
2023-06-29 22:46:41 +0100

    chore: typeorm database module update

--- src/infrastructure/database/typeorm-database.module.ts
+++ src/infrastructure/database/typeorm-database.module.ts
@@ -7,11 +7,12 @@ import { ClienteEntity } from './cliente/entity/cliente.entity';
 import { ClienteTypeormRepository } from './cliente/repository/cliente-typeorm.repository';
 import { ProdutoEntity } from './produto/entity/produto.entity';
 import { ProdutoTypeormRepository } from './produto/repository/produto-typeorm.repository';
-import { PedidoConstants } from 'src/shared/constants';
+import { ItemPedidoConstants, PedidoConstants } from 'src/shared/constants';
 import { PedidoEntity } from './pedido/entity/pedido.entity';
 import { PedidoTypeormRepository } from './pedido/repository/pedido-typeorm.repository';
 import { CategoriaProdutoTypeormRepository } from './categoria/repository/categoria-produto-typeorm.repository';
 import { CategoriaProdutoEntity } from './categoria/entity/categoria-produto.entity';
+import { ItemPedidoTypeormRepository } from './item-pedido/repository/item-pedido-typeorm.repository';
 
 @Module({
    imports: [
@@ -28,12 +29,14 @@ import { CategoriaProdutoEntity } from './categoria/entity/categoria-produto.ent
       { provide: PedidoConstants.IREPOSITORY, useClass: PedidoTypeormRepository },
       { provide: 'IRepository<Produto>', useClass: ProdutoTypeormRepository },
       { provide: 'IRepository<CategoriaProduto>', useClass: CategoriaProdutoTypeormRepository },
+      { provide: ItemPedidoConstants.IREPOSITORY, useClass: ItemPedidoTypeormRepository },
    ],
    exports: [
       { provide: 'IRepository<Cliente>', useClass: ClienteTypeormRepository },
       { provide: PedidoConstants.IREPOSITORY, useClass: PedidoTypeormRepository },
       { provide: 'IRepository<Produto>', useClass: ProdutoTypeormRepository },
       { provide: 'IRepository<CategoriaProduto>', useClass: CategoriaProdutoTypeormRepository },
+      { provide: ItemPedidoConstants.IREPOSITORY, useClass: ItemPedidoTypeormRepository },
    ],
 })
 export class TypeormDatabaseModule {}
