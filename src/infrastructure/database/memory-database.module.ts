2f0bdfb3192621d02332b56f774722763db5f1ca
Felipe Maximiliano da Rosa <fmo19@bskyb.com>
2023-06-29 22:45:49 +0100

    chore: memory database module update

--- src/infrastructure/database/memory-database.module.ts
+++ src/infrastructure/database/memory-database.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 
 import { ClienteMemoryRepository } from './cliente/repository/cliente-memory.repository';
 import { PedidoMemoryRepository } from './pedido/repository/pedido-memory.repository';
-import { PedidoConstants } from 'src/shared/constants';
+import { ItemPedidoConstants, PedidoConstants } from 'src/shared/constants';
 import { ProdutoMemoryRepository } from './produto/repository/produto-memory.repository';
 import { CategoriaProdutoMemoryRepository } from './categoria/repository/categoria-produto-memory.repository';
+import { ItemPedidoMemoryRepository } from './item-pedido/repository/item-pedido-memory.repository';
 
 @Module({
    providers: [
@@ -12,12 +13,14 @@ import { CategoriaProdutoMemoryRepository } from './categoria/repository/categor
       { provide: PedidoConstants.IREPOSITORY, useClass: PedidoMemoryRepository },
       { provide: 'IRepository<Produto>', useClass: ProdutoMemoryRepository },
       { provide: 'IRepository<CategoriaProduto>', useClass: CategoriaProdutoMemoryRepository },
+      { provide: ItemPedidoConstants.IREPOSITORY, useClass: ItemPedidoMemoryRepository },
    ],
    exports: [
       { provide: 'IRepository<Cliente>', useClass: ClienteMemoryRepository },
       { provide: PedidoConstants.IREPOSITORY, useClass: PedidoMemoryRepository },
       { provide: 'IRepository<Produto>', useClass: ProdutoMemoryRepository },
       { provide: 'IRepository<CategoriaProduto>', useClass: CategoriaProdutoMemoryRepository },
+      { provide: ItemPedidoConstants.IREPOSITORY, useClass: ItemPedidoMemoryRepository },
    ],
 })
 export class MemoryDatabaseModule {}
