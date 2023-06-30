003b04d6f937999b175d3cf0d1978fbcf3f9cc63
Renato Rodrigues <spamcares-github@yahoo.com>
2023-06-29 23:29:34 -0300

    - Ajustes de versão e descrições

--- src/application/constants/application.constants.ts
+++ src/application/constants/application.constants.ts
@@ -1,7 +1,7 @@
 export class ApplicationConstants {
    //Swaggwer
    public static readonly SWAGGER_PATH: string = 'api';
-   public static readonly SWAGGER_TITLE: string = 'Fast and Foodious API Server';
-   public static readonly SWAGGER_VERSION: string = '0.0.1';
-   public static readonly SWAGGER_DESCRIPTION: string = 'Fast and Foodious Rest API';
+   public static readonly SWAGGER_TITLE: string = '🍔 Fast & Foodious API';
+   public static readonly SWAGGER_VERSION: string = '1.0.0-SNAPSHOT';
+   public static readonly SWAGGER_DESCRIPTION: string = 'Sistema de auto-atendimento de fast food. Projeto de conclusão da Fase 01 da pós gradução em Software Architecture.';
 }
