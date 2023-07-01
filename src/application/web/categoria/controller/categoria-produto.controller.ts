ea529611e4c8dbffd710123f7ef080b025d78829
Renato Rodrigues <spamcares-github@yahoo.com>
2023-07-01 11:56:52 -0300

    - Implementação de lista categoria response

--- src/application/web/categoria/controller/categoria-produto.controller.ts
+++ src/application/web/categoria/controller/categoria-produto.controller.ts
@@ -7,22 +7,27 @@ import { ListarCategoriaResponse } from './response/listar-categoria.response';
 
 @Controller('v1/categoria')
 @ApiTags('Categoria Produto')
-export class CategoriaProdutoController extends BaseController{
+export class CategoriaProdutoController extends BaseController {
    private logger: Logger = new Logger(CategoriaProdutoController.name);
 
-   constructor(@Inject('IService<CategoriaProduto>') private service: ICategoriaProdutoService) {super()}
+   constructor(@Inject('IService<CategoriaProduto>') private service: ICategoriaProdutoService) {
+      super();
+   }
 
    @Get()
    @ApiOperation({
       summary: 'Lista categorias',
       description: 'Lista categorias de produto',
    })
-   @ApiOkResponse({ description: 'Categorias de produto encontradas com sucesso', type: ListarCategoriaResponse, isArray: true })
+   @ApiOkResponse({
+      description: 'Categorias de produto encontradas com sucesso',
+      type: ListarCategoriaResponse,
+      isArray: true,
+   })
    async findAll(): Promise<CategoriaProduto[]> {
       this.logger.debug(`Listando todas as categorias de produto`);
-      return await this.service.findAll()
-         .then((categorias) => {
-            return categorias.map((categoria) => new ListarCategoriaResponse(categoria));
-         });
+      return await this.service.findAll().then((categorias) => {
+         return categorias.map((categoria) => new ListarCategoriaResponse(categoria));
+      });
    }
 }
--- src/application/web/categoria/controller/response/listar-categoria.response.ts
+++ src/application/web/categoria/controller/response/listar-categoria.response.ts
@@ -1,17 +1,15 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { CategoriaProduto } from "src/domain/categoria/model/categoria-produto.model";
+import { ApiProperty } from '@nestjs/swagger';
+import { CategoriaProduto } from 'src/domain/categoria/model/categoria-produto.model';
 
 export class ListarCategoriaResponse {
+   @ApiProperty({ required: true, nullable: false, description: 'Nome da categoria' })
+   public nome: string;
 
-    @ApiProperty({ required: true, nullable: false, description: 'Nome da categoria' })
-    public nome: string;
+   @ApiProperty({ required: true, nullable: false, description: 'ID da categoria' })
+   public id: number;
 
-    @ApiProperty({ required: true, nullable: false, description: 'ID da categoria' })
-    public id: number;
-
-    constructor(categoriaProduto: CategoriaProduto) {
-        this.id = categoriaProduto.id;
-        this.nome = categoriaProduto.nome;
-    }
- }
- 
+   constructor(categoriaProduto: CategoriaProduto) {
+      this.id = categoriaProduto.id;
+      this.nome = categoriaProduto.nome;
+   }
+}
