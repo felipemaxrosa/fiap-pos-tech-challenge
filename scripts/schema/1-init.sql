09b7daf4e6dcd12c4c1c4d9232e8ca0a64e08fc7
Felipe Rosa <felipemax.suporteti@gmail.com>
2023-08-25 22:04:51 +0100

    refactor: removing extra commas

--- scripts/schema/1-init.sql
+++ scripts/schema/1-init.sql
@@ -70,5 +70,5 @@ CREATE TABLE IF NOT EXISTS PAGAMENTO (
                                        TRANSACAO_ID INT NOT NULL,
                                        ESTADO_PAGAMENTO INT NOT NULL,
                                        TOTAL DECIMAL(8,2) NOT NULL,
-                                       DATA_HORA_PAGAMENTO DATETIME NOT NULL,
+                                       DATA_HORA_PAGAMENTO DATETIME NOT NULL
 );
