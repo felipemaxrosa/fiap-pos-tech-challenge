9fc53f3347846cf7afdc125c5c5e0c9bb3fdae6a
Felipe Rosa <felipemax.suporteti@gmail.com>
2023-08-27 13:45:15 +0100

    feat: pagamento repositories

--- src/infrastructure/persistence/providers/persistence-in-memory.providers.ts
+++ src/infrastructure/persistence/providers/persistence-in-memory.providers.ts
@@ -6,12 +6,14 @@ import {
    ItemPedidoConstants,
    PedidoConstants,
    ProdutoConstants,
+   PagamentoConstants,
 } from 'src/shared/constants';
 import { CategoriaProdutoMemoryRepository } from 'src/infrastructure/persistence/categoria/repository/categoria-produto-memory.repository';
 import { ClienteMemoryRepository } from 'src/infrastructure/persistence/cliente/repository/cliente-memory.repository';
 import { ItemPedidoMemoryRepository } from 'src/infrastructure/persistence/item-pedido/repository/item-pedido-memory.repository';
 import { PedidoMemoryRepository } from 'src/infrastructure/persistence/pedido/repository/pedido-memory.repository';
 import { ProdutoMemoryRepository } from 'src/infrastructure/persistence/produto/repository/produto-memory.repository';
+import { PagamentoMemoryRepository } from 'src/infrastructure/persistence/pagamento/repository/pagamento-memory.repository';
 
 export const PersistenceInMemoryProviders: Provider[] = [
    { provide: ClienteConstants.IREPOSITORY, useClass: ClienteMemoryRepository },
@@ -19,4 +21,5 @@ export const PersistenceInMemoryProviders: Provider[] = [
    { provide: ProdutoConstants.IREPOSITORY, useClass: ProdutoMemoryRepository },
    { provide: CategoriaProdutoConstants.IREPOSITORY, useClass: CategoriaProdutoMemoryRepository },
    { provide: ItemPedidoConstants.IREPOSITORY, useClass: ItemPedidoMemoryRepository },
+   { provide: PagamentoConstants.IREPOSITORY, useClass: PagamentoMemoryRepository },
 ];
