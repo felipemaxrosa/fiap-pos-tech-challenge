28270ee5a642b9dda47019860b848564e8066378
Felipe Rosa <felipemax.suporteti@gmail.com>
2023-08-27 16:30:54 +0100

    feat: pagamento api atualizada

--- src/presentation/rest/pagamento/api/pagamento.api.spec.ts
+++ src/presentation/rest/pagamento/api/pagamento.api.spec.ts
@@ -1,13 +1,37 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { IPagamentoService } from 'src/application/pagamento/service/pagamento.service.interface';
 import { PagamentoRestApi } from 'src/presentation/rest/pagamento/api/pagamento.api';
+import { BuscarEstadoPagamentoPedidoRequest } from '../request';
+import { BuscarEstadoPagamentoPedidoResponse } from '../response';
+import { EstadoPagamento } from 'src/enterprise/pagamento/enums/pagamento.enums';
+import { PagamentoConstants } from 'src/shared/constants';
 
 describe('PagamentoRestApi', () => {
    let restApi: PagamentoRestApi;
+   let service: IPagamentoService;
+
+   const request: BuscarEstadoPagamentoPedidoRequest = {
+      pedidoId: 1,
+   };
+
+   const response: BuscarEstadoPagamentoPedidoResponse = {
+      estadoPagamento: EstadoPagamento.CONFIRMADO,
+   };
 
    beforeEach(async () => {
       // Configuração do módulo de teste
       const module: TestingModule = await Test.createTestingModule({
          controllers: [PagamentoRestApi],
+         providers: [
+            {
+               provide: PagamentoConstants.ISERVICE,
+               useValue: {
+                  buscarEstadoPagamentoPedido: jest.fn((pedidoId) =>
+                     pedidoId === 1 ? Promise.resolve(response) : Promise.reject(new Error('error')),
+                  ),
+               },
+            },
+         ],
       }).compile();
 
       // Desabilita a saída de log
@@ -15,6 +39,14 @@ describe('PagamentoRestApi', () => {
 
       // Obtém a instância do restApi a partir do módulo de teste
       restApi = module.get<PagamentoRestApi>(PagamentoRestApi);
+      service = module.get<IPagamentoService>(PagamentoConstants.ISERVICE);
+   });
+
+   describe('injeção de dependências', () => {
+      it('deve existir instância de serviço definida', async () => {
+         // Verifica se a instância de serviço está definida
+         expect(service).toBeDefined();
+      });
    });
 
    describe('efetuar pagamento', () => {
@@ -24,5 +56,14 @@ describe('PagamentoRestApi', () => {
          // Verifica se o resultado obtido é igual ao esperado
          expect(result).toBeTruthy();
       }); // end it 'o pagamento deve ser realizado com sucesso'
+   });
+
+   describe('consultar estado do pagamento por ID do pedido', () => {
+      it('a consulta deve ser realizada com sucesso', async () => {
+         const result = await restApi.buscarPorPedidoId(request);
+
+         expect(service.buscarEstadoPagamentoPedido).toHaveBeenCalledTimes(1);
+         expect(result).toEqual(response);
+      });
    });
 });
--- src/presentation/rest/pagamento/api/pagamento.api.ts
+++ src/presentation/rest/pagamento/api/pagamento.api.ts
@@ -1,11 +1,29 @@
-import { Controller, Logger, Param, ParseIntPipe, Post } from '@nestjs/common';
+import {
+   Inject,
+   Controller,
+   Logger,
+   Param,
+   ParseIntPipe,
+   Post,
+   Get,
+   Query,
+   ValidationPipe,
+   NotFoundException,
+} from '@nestjs/common';
 import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { IPagamentoService } from 'src/application/pagamento/service/pagamento.service.interface';
 import { BaseRestApi } from 'src/presentation/rest/base.api';
-
+import { PagamentoConstants } from 'src/shared/constants';
+import { BuscarEstadoPagamentoPedidoResponse } from 'src/presentation/rest/pagamento/response';
+import { BuscarEstadoPagamentoPedidoRequest } from 'src/presentation/rest/pagamento/request';
 @Controller('v1/pagamento')
 @ApiTags('Pagamento')
 export class PagamentoRestApi extends BaseRestApi {
    private logger: Logger = new Logger(PagamentoRestApi.name);
+
+   constructor(@Inject(PagamentoConstants.ISERVICE) private service: IPagamentoService) {
+      super();
+   }
 
    @Post(':id')
    @ApiOperation({
@@ -19,5 +37,27 @@ export class PagamentoRestApi extends BaseRestApi {
    async pagar(@Param('id', ParseIntPipe) id: number): Promise<boolean> {
       this.logger.debug('Pagamento realizado com sucesso para o pedido ' + id + '.');
       return Promise.resolve(true);
+   }
+
+   @Get('estado')
+   @ApiOperation({
+      summary: 'Consulta estado do pagamento por ID do Pedido',
+      description: 'Realiza consulta do estado do pagamento por ID do Pedido',
+   })
+   @ApiOkResponse({
+      description: 'Estado do pagamento consultado com sucesso',
+      type: BuscarEstadoPagamentoPedidoResponse,
+   })
+   async buscarPorPedidoId(
+      @Query(ValidationPipe) query: BuscarEstadoPagamentoPedidoRequest,
+   ): Promise<BuscarEstadoPagamentoPedidoResponse> {
+      this.logger.debug(`Consultando estado do pagamento por ID do pedido: ${query}`);
+      return await this.service.buscarEstadoPagamentoPedido(query.pedidoId).then((pagamento) => {
+         if (pagamento === undefined) {
+            throw new NotFoundException('Cliente não encontrado');
+         }
+
+         return new BuscarEstadoPagamentoPedidoResponse(pagamento);
+      });
    }
 }
