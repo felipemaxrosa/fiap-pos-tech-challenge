16fee96eea5697adf2362a0cbe40a34f82fbf7e4
Felipe Rosa <felipemax.suporteti@gmail.com>
2023-08-27 15:33:05 +0100

    feat: pagamento service applied on application module

--- src/application/application.module.ts
+++ src/application/application.module.ts
@@ -5,6 +5,7 @@ import { ClienteService } from 'src/application/cliente/service/cliente.service'
 import { ItemPedidoService } from 'src/application/item-pedido/service/item-pedido.service';
 import { PedidoService } from 'src/application/pedido/service/pedido.service';
 import { ProdutoService } from 'src/application/produto/service/produto.service';
+import { PagamentoService } from './pagamento/service/pagamento.service';
 import { ClienteProviders } from 'src/application/cliente/providers/cliente.providers';
 import { PedidoProviders } from 'src/application/pedido/providers/pedido.providers';
 import { ProdutoProviders } from 'src/application/produto/providers/produto.providers';
@@ -16,6 +17,7 @@ import {
    ProdutoConstants,
    ItemPedidoConstants,
    CategoriaProdutoConstants,
+   PagamentoConstants,
 } from 'src/shared/constants';
 
 @Module({
@@ -32,6 +34,7 @@ import {
       { provide: ItemPedidoConstants.ISERVICE, useClass: ItemPedidoService },
       { provide: ProdutoConstants.ISERVICE, useClass: ProdutoService },
       { provide: CategoriaProdutoConstants.ISERVICE, useClass: CategoriaProdutoService },
+      { provide: PagamentoConstants.ISERVICE, useClass: PagamentoService },
    ],
 })
 export class ApplicationModule {}
