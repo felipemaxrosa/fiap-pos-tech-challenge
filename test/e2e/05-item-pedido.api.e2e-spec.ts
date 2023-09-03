e5e7bd9f5b3b84b87f224be49d887b05ab59d4af
Felipe Rosa <felipemax.suporteti@gmail.com>
2023-09-03 14:15:36 +0100

    test: criando cliente e pedido antes de adicionar item

--- test/e2e/05-item-pedido.api.e2e-spec.ts
+++ test/e2e/05-item-pedido.api.e2e-spec.ts
@@ -3,11 +3,25 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { MainModule } from 'src/main.module';
 import { SalvarItemPedidoRequest, SalvarItemPedidoResponse } from 'src/presentation/rest/item-pedido';
+import { EstadoPedido } from 'src/enterprise/pedido/enums/pedido';
 
 describe('ItemPedidoRestApi (e2e)', () => {
    let app: INestApplication;
    let salvarItemPedidoRequest: SalvarItemPedidoRequest;
    let itemPedido: SalvarItemPedidoResponse;
+
+   const salvarClienteRequest = {
+      nome: 'Teste',
+      email: 'teste@teste.com',
+      cpf: '25634428777',
+   };
+
+   const salvarPedidoRequest = {
+      clienteId: 1,
+      dataInicio: '2023-06-24',
+      estadoPedido: EstadoPedido.PAGAMENTO_PENDENTE,
+      ativo: true,
+   };
 
    beforeEach(() => {
       salvarItemPedidoRequest = {
@@ -38,6 +52,20 @@ describe('ItemPedidoRestApi (e2e)', () => {
       // Configuração de validações global inputs request
       app.useGlobalPipes(new ValidationPipe({ stopAtFirstError: true }));
       await app.init();
+
+      // Criar cliente e Pedido
+      //TODO: remover quando reutilizar TestingModule entre os testes
+      if (process.env.NODE_ENV === 'local-mock-repository') {
+         await request(app.getHttpServer())
+            .post('/v1/cliente')
+            .set('Content-type', 'application/json')
+            .send(salvarClienteRequest);
+
+         await request(app.getHttpServer())
+            .post('/v1/pedido')
+            .set('Content-type', 'application/json')
+            .send(salvarPedidoRequest);
+      }
    });
 
    afterAll(async () => {
