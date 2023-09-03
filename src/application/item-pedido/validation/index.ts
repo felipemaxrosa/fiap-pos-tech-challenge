efbedb44ab7086480da5abfa1de1d9e42142bb38
Felipe Rosa <felipemax.suporteti@gmail.com>
2023-09-03 13:43:27 +0100

    fix: valida se pedido existe

--- src/application/item-pedido/validation/index.ts
+++ src/application/item-pedido/validation/index.ts
@@ -2,3 +2,4 @@ export * from './add-item-pedido.validator';
 export * from './quantidade-minima-item.validator';
 export * from './editar-item-pedido.validator';
 export * from './item-pedido-existe.validator';
+export * from './pedido-existe.validator';
